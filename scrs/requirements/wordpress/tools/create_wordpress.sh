d92b21ca859d57c1e94abda2d8b49024305e08c8
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-14 21:41:05 +0100

    all

--- scrs/requirements/wordpress/tools/create_wordpress.sh
+++ scrs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-#check if wp-config.php exist
 if [ -f ./wp-config.php ]
 then
-	echo "wordpress already downloaded"
+	echo "wordpress exist"
 else
-
-####### MANDATORY PART ##########
 
 	#Download wordpress and all config file
 	wget http://wordpress.org/latest.tar.gz
@@ -15,15 +12,11 @@ else
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
-	#Inport env variables in the config file
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
-###################################
-
-####### BONUS PART ################
 
 ## redis ##
 
